018ccbe716784941f6c75f688d5ec953af00a7fc
dci-student <hugon1992@gmail.com>
2024-04-08 20:03:23 +0200

    Hero,Item,Popular Components created styled, Popular to Shop added

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "../Components/Hero/Hero.jsx";
+import Popular from "../Components/Popular/Popular.jsx";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <Hero />
+      <Popular />
+    </div>
+  );
 };
 
 export default Shop;
