5d8ab79214c906a4c2f1c15bfd79b44d40a6a196
dci-student <hugon1992@gmail.com>
2024-04-08 15:08:12 +0200

    added Components to Pages-routes created, linked to navbar-react-router-dom installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Shop from "./Pages/Shop.jsx";
+import ShopCategory from "./Pages/ShopCategory.jsx";
+import Product from "./Pages/Product.jsx";
+import Cart from "./Pages/Cart.jsx";
+import LoginSignUp from "./Pages/LoginSignUp.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
