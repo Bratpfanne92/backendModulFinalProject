9223b6e0fb11f906ae739d771b8cf0234cad5865
dci-student <hugon1992@gmail.com>
2024-04-09 15:47:29 +0200

    db connected,tested with insomni, rsponse ok. Multer upload success-tested, endpoint created for newly added products(/addproduct)- Schema for addProduct created(required added)

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,98 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const exp = require("constants");
+const { default: mongoose } = require("mongoose");
 app.use(express.json()); //parse true json
 app.use(cors()); //connect frontend and backend to 4000 port
-const mongoose = require("mongoose");
+
 const port = process.env.PORT || 3000;
+require("./dbconnect");
 
 //API creation
 
 app.get("/", (req, res) => {
   res.send("Express App is running!-response ok");
+});
+
+// Image Storage Engine-MULTER
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload for Endpoint Image
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    //access to uploaded image
+  });
+});
+
+// Schema for Creating Products
+
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+//Endpoint for addproduct-save to db
+app.post("/addproduct", async (req, res) => {
+  const product = new Product({
+    id: req.body.id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+    // available: req.body.available,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Saved to DB");
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
 });
 
 //App listening
