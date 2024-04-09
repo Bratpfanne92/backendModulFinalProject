de42ffcccd23d55c43daf99fef4732d01ae15790
dci-student <hugon1992@gmail.com>
2024-04-09 09:51:47 +0200

    padding edit

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPix</p>
+        <p>ShopX</p>
       </div>
       <ul className="nav-menu">
         <li
