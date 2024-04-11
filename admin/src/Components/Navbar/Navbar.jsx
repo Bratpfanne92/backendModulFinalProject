07b1f3b3bf11304e157684d69995e596e5e15d07
dci-student <hugon1992@gmail.com>
2024-04-11 11:53:04 +0200

    Titel added to NavBar

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import navProfile from "../../assets/nav-profile.svg";
 const Navbar = () => {
   return (
     <div className="navbar">
+      <h1>ADMIN PANEL</h1>
       {/* <img src={navlogo} alt="" className="nav-logo" /> */}
       <img src={navProfile} alt="" className="nav-profile" />
     </div>
