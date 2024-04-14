deb9c7bb764bea671a3e6f125e3d4d18d494b19c
dci-student <hugon1992@gmail.com>
2024-04-14 12:21:39 +0200

    Products added to db

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createContext } from "react";
-import all_product from "../Components/Assets/all_product";
+// import all_product from "../Components/Assets/all_product";
+import { useState } from "react";
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 0; i < 300 + 1; i++) {
+    cart[i] = 0;
+  }
+};
+
 const ShopContextProvider = (props) => {
+  const [all_product, setAll_Product] = useState([]);
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  useEffect(() => {
+    fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => setAll_Product(data));
+  }, []);
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => {
+      return { ...prev, [itemId]: prev[itemId] + 1 };
+    });
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] - 1,
+    }));
+  };
+
   const contextValue = { all_product };
 
   return (
