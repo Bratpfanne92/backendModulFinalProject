532647d071a4c8dcaba7de804305d42140923776
dci-student <hugon1992@gmail.com>
2024-04-15 12:04:08 +0200

    Created Product Page Component- for navigation im menü created a Breadcrum Component and for Product Description and SizeChange created a ProductDisplay.jsx. Both added to Product.jsx Component-without style

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext.jsx";
+import Breadcrum from "../Components/Breadcrums/Breadcrum.jsx";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay.jsx";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 
 export default Product;
